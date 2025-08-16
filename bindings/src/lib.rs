7184fed82fa2d9316d0797a72d161becfa1aee3d
Nate Jones <nate@uint8.dev>
2025-08-16 12:19:01 -0700

    A bit more work

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,3 +1,17 @@
+#![allow(unused)]
+
 mod bindings;
 
 pub use bindings::*;
+use windows::Win32::System::LibraryLoader::{
+    LoadLibraryW,
+    GetProcAddress,
+};
+use windows_strings::*;
+
+fn blah() {
+    unsafe {
+        let _mod_handle = LoadLibraryW(w!("webauthn.dll")).expect("Loading webauthn.dll failed");
+        //let addr = GetProcAddress(mod_handle, w!("")).expect("Couldn't find func");
+    }
+}
