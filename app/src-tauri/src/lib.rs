a35493f83767a5780e1c1066eac89f95d6bade7a
Nate Jones <nate@uint8.dev>
2025-09-27 20:41:48 -0700

    A bit of work with some new binding infra, move to yarn

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    use bindings::Windows::PluggablePasskeys::EXPERIMENTAL_WebAuthNDecodeGetAssertionRequestFn;
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
--- gen-bindings/src/main.rs
+++ gen-bindings/src/main.rs
@@ -5,19 +5,14 @@ fn main() {
 
 #[cfg(target_os = "windows")]
 fn windows() {
-    println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-changed=.windows\\pluginauthenticator.winmd");
-
-    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not set");
-
-    let output_file = format!("{}\\windows_plugin_authenticator_bindings.rs", out_dir);
-    let args: [&str; 8] = [
+    let output_file = format!("..\\bindings\\src\\bindings.rs");
+    let args: [&str; _] = [
         "--in",
         "default",
         ".windows/pluginauthenticator.winmd",
+        "--sys",
         "--filter",
         "Windows.PluggablePasskeys",
-        "--no-allow",
         "--out",
         &output_file,
     ];
